3e940baea136cedd3cc4dbe137ed88e0f069e564
Poloko <polokoserero@gmail.com>
2023-05-15 03:38:58 +0200

    get products in catergory function

--- functions.php
+++ functions.php
@@ -1,2 +1,22 @@
 <?php
 require_once('src/autoload.php');
+
+/**
+ * Return a products inside a category.
+ *
+ * @param string $category
+ *
+ * @return array
+ */
+function getProductsInCategory(string $category, array $data): array {
+    // Loop through data
+    for ($x = 0; $x <= (count($data) - 1); $x++) {
+        //check for needed category
+        if($data[$x]->name == $category){
+            //If true return products
+            return $data[$x]->products;
+        }
+    }
+    return [];
+}
+   
